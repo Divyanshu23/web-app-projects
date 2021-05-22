7392e64ad4f48605120d17d40586705806812cd1
Divyanshu23 <dvyanshu@iitk.ac.in>
2021-05-22 21:21:13 +0530

    Add first Project

---
+++ YearEndCounter/js/main.js
@@ -0,0 +1,36 @@
+let day = document.getElementById("days")
+let hour = document.getElementById("hours")
+let min = document.getElementById("minutes")
+let sec = document.getElementById("seconds")
+let id = undefined
+
+const endTime = new Date("December 31 2021 23:59:59")
+
+const updateCountdown = () => {
+  const currTime = new Date()
+  diff = endTime - currTime
+  days = Math.floor((((diff/1000)/60)/60)/24)
+  hours = Math.floor(((diff/1000)/60)/60)
+  hours %= 24
+  minutes = Math.floor((diff/1000)/60)
+  minutes %= 60
+  seconds = Math.floor(diff/1000)
+  seconds %= 60
+
+  day.innerHTML = days
+  hour.innerHTML = hours
+  min.innerHTML = minutes
+  sec.innerHTML = seconds
+} 
+
+document.getElementById("start").onclick = startCounter
+
+function startCounter() {
+  id = setInterval(updateCountdown,1000)
+}
+
+document.getElementById("stop").onclick = stopCounter
+
+function stopCounter() {
+  clearInterval(id);
+}
